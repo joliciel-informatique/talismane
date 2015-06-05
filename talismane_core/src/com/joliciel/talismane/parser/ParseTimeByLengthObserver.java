03f06172352086b53d7632c9c2139cb0f83decd9
Assaf Urieli <assaf.urieli@gmail.com>
2015-06-05 23:01:00 +0200

    Release 2.4.1.8b Added option includeTimePerToken to create separate file with analysis time per token by sentence length stats.

--- talismane_core/src/com/joliciel/talismane/parser/ParseTimeByLengthObserver.java
+++ talismane_core/src/com/joliciel/talismane/parser/ParseTimeByLengthObserver.java
@@ -1,5 +1,7 @@
 package com.joliciel.talismane.parser;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
@@ -10,11 +12,16 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import com.joliciel.talismane.posTagger.PosTagSequence;
+import com.joliciel.talismane.utils.CSVFormatter;
+import com.joliciel.talismane.utils.LogUtils;
 
 public class ParseTimeByLengthObserver implements ParseEvaluationObserver {
 	private static final Log LOG = LogFactory.getLog(ParseTimeByLengthObserver.class);
+	private static final CSVFormatter CSV = new CSVFormatter();
 	private Map<Integer, DescriptiveStatistics> timeStatsPerLength = new TreeMap<Integer, DescriptiveStatistics>();
 
+	private Writer writer = null;
+	
 	private long startTime;
 	@Override
 	public void onParseStart(ParseConfiguration realConfiguration,
@@ -37,26 +44,48 @@ public class ParseTimeByLengthObserver implements ParseEvaluationObserver {
 
 	@Override
 	public void onEvaluationComplete() {
-		LOG.info("##################");
-		LOG.info("timeStatsPerLength");
-		LOG.info("\tlength\tcount\tmean\tperToken");
-		DecimalFormat df = new DecimalFormat("0.00");
-		for (int length : timeStatsPerLength.keySet()) {
-			DescriptiveStatistics stats = timeStatsPerLength.get(length);
-			long count = stats.getN();
-			double mean = stats.getMean();
-			double perToken = mean / length;
-			LOG.info("\t" + length + "\t" + count + "\t" + df.format(mean) + "\t" + df.format(perToken));
+		try {
+			LOG.info("##################");
+			LOG.info("timeStatsPerLength");
+			LOG.info("\tlength\tcount\tmean\tperToken");
+			if (writer!=null)
+				writer.write(CSV.format("length") + CSV.format("count") + CSV.format("mean") + CSV.format("perToken") + "\n");
+			DecimalFormat df = new DecimalFormat("0.00");
+			for (int length : timeStatsPerLength.keySet()) {
+				DescriptiveStatistics stats = timeStatsPerLength.get(length);
+				long count = stats.getN();
+				double mean = stats.getMean();
+				double perToken = mean / length;
+				LOG.info("\t" + length + "\t" + count + "\t" + df.format(mean) + "\t" + df.format(perToken));
+				if (writer!=null)
+					writer.write(CSV.format(length) + CSV.format(count) + CSV.format(mean) + CSV.format(perToken) + "\n");
+			}
+			if (writer!=null) {
+				writer.flush();
+				writer.close();
+			}
+			LOG.info("##################");
+		} catch (IOException e) {
+			LogUtils.logError(LOG, e);
+			throw new RuntimeException(e);
 		}
-		LOG.info("##################");
 	}
 	
 	/**
 	 * Total parsing time statistics for sentences of different lengths.
 	 * @return
 	 */
-
 	public Map<Integer, DescriptiveStatistics> getTimeStatsPerLength() {
 		return timeStatsPerLength;
 	}
+
+	public Writer getWriter() {
+		return writer;
+	}
+
+	public void setWriter(Writer writer) {
+		this.writer = writer;
+	}
+	
+	
 }
