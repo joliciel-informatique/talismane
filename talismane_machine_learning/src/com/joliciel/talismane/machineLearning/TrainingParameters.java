f46cf32276478538072c625d9f2f715a6ae17992
Assaf Urieli <assaf.urieli@gmail.com>
2015-12-07 17:20:26 +0100

    Release 2.4.7.4b Added new constructor for TrainingParameters, possibility of setting outputEncoding in FScoreCalculator

--- talismane_machine_learning/src/com/joliciel/talismane/machineLearning/TrainingParameters.java
+++ talismane_machine_learning/src/com/joliciel/talismane/machineLearning/TrainingParameters.java
@@ -86,6 +86,16 @@ public class TrainingParameters {
 		this.prefix = prefix;
 	}
 	
+	public TrainingParameters(String prefix, File baseDir, Map<String, String> props) {
+		this(prefix);
+		this.load(baseDir, props);
+	}
+	
+	public TrainingParameters(TrainingParameters defaultParameters, String prefix, File baseDir, Map<String, String> props) {
+		this(defaultParameters, prefix);
+		this.load(baseDir, props);
+	}
+	
 	/**
 	 * Load a given set of properties with paths relative to a given base directory,
 	 * and return a set of all property names processed.
