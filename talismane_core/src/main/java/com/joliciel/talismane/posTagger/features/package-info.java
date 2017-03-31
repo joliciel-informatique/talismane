ec6bf78a3240d9c2d54c5c91add9c03cd7614dd6
Assaf Urieli <assaf.urieli@gmail.com>
2017-03-31 15:55:21 +0200

    Fix CLI options for text and sentence annotators

--- talismane_core/src/main/java/com/joliciel/talismane/posTagger/features/package-info.java
+++ talismane_core/src/main/java/com/joliciel/talismane/posTagger/features/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Various features specific to the pos-tagging phase.<br/>
  * The available feature descriptors are listed in
- * {@link com.joliciel.talismane.posTagger.features.PosTaggerFeatureParser#addFeatureClasses(com.joliciel.talismane.machineLearning.features.FeatureClassContainer))}
+ * {@link com.joliciel.talismane.posTagger.features.PosTaggerFeatureParser#addFeatureClasses(com.joliciel.talismane.machineLearning.features.FeatureClassContainer)}
  */
 
 package com.joliciel.talismane.posTagger.features;
--- talismane_core/src/main/java/com/joliciel/talismane/tokeniser/features/package-info.java
+++ talismane_core/src/main/java/com/joliciel/talismane/tokeniser/features/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Features specific to the tokenisation phase.<br/>
  * The available feature descriptors are listed in
- * {@link com.joliciel.talismane.tokeniser.features.TokenPatternMatchFeatureParser#addFeatureClasses(com.joliciel.talismane.machineLearning.features.FeatureClassContainer)))}
+ * {@link com.joliciel.talismane.tokeniser.features.TokenPatternMatchFeatureParser#addFeatureClasses(com.joliciel.talismane.machineLearning.features.FeatureClassContainer)}
  */
 package com.joliciel.talismane.tokeniser.features;
