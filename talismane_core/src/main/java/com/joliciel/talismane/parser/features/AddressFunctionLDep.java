8a87f72b42c01317f4f7e7fc5a5ef0826cb5daec
Assaf Urieli <assaf.urieli@gmail.com>
2017-05-12 12:35:06 +0200

    Enable correct handling of both projective and non-projective arcs
    By creating methods on ParseConfiguration which allow reading of
    non-projective arc info.
    
    We also simplified the internal implementation of ParseConfiguration to
    update all state-related attributes (used to improve peformance) each time
    a new projective or non-projective arc is generated. This affects the methods
    used to add and remove dependencies.

--- talismane_core/src/main/java/com/joliciel/talismane/parser/features/AddressFunctionLDep.java
+++ talismane_core/src/main/java/com/joliciel/talismane/parser/features/AddressFunctionLDep.java
@@ -18,7 +18,7 @@
 //////////////////////////////////////////////////////////////////////////////
 package com.joliciel.talismane.parser.features;
 
-import java.util.List;
+import java.util.NavigableSet;
 
 import com.joliciel.talismane.TalismaneException;
 import com.joliciel.talismane.machineLearning.features.FeatureResult;
@@ -50,9 +50,9 @@ public final class AddressFunctionLDep extends AbstractAddressFunction {
     FeatureResult<PosTaggedTokenWrapper> addressResult = addressFunction.check(wrapper, env);
     if (addressResult != null) {
       PosTaggedToken referenceToken = addressResult.getOutcome().getPosTaggedToken();
-      List<PosTaggedToken> leftDependents = configuration.getLeftDependents(referenceToken);
+      NavigableSet<PosTaggedToken> leftDependents = configuration.getLeftDependents(referenceToken);
       if (leftDependents.size() > 0)
-        resultToken = leftDependents.get(0);
+        resultToken = leftDependents.first();
     }
 
     FeatureResult<PosTaggedTokenWrapper> featureResult = null;
