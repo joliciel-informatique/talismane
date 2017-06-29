bf34b87dd7d51c5a038d7155fba214350967c534
Assaf Urieli <assaf.urieli@gmail.com>
2017-06-29 14:50:35 +0200

    Change LemmaFeature to a single Lemma, create separate AllLemmas feature

--- talismane_core/src/main/java/com/joliciel/talismane/posTagger/features/AllLemmasFeature.java
+++ talismane_core/src/main/java/com/joliciel/talismane/posTagger/features/AllLemmasFeature.java
@@ -21,46 +21,28 @@ package com.joliciel.talismane.posTagger.features;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.joliciel.talismane.TalismaneException;
 import com.joliciel.talismane.lexicon.LexicalAttribute;
-import com.joliciel.talismane.lexicon.LexicalEntry;
-import com.joliciel.talismane.machineLearning.features.FeatureResult;
 import com.joliciel.talismane.machineLearning.features.RuntimeEnvironment;
-import com.joliciel.talismane.machineLearning.features.StringFeature;
-import com.joliciel.talismane.posTagger.PosTaggedToken;
+import com.joliciel.talismane.machineLearning.features.StringCollectionFeature;
 
 /**
- * The first lemma (where first is arbitrary, but will always be the same for a
- * given word form) of a given pos-tagged token as supplied by the lexicon.
+ * All of the lemmas for a given pos-tagged token as supplied by the lexicon.
  * 
  * @author Assaf Urieli
  *
  */
-public final class LemmaFeature<T> extends AbstractPosTaggedTokenFeature<T, String>implements StringFeature<T> {
+public final class AllLemmasFeature<T> extends AbstractLexicalAttributeFeature<T>implements StringCollectionFeature<T> {
   private final List<String> attributes = new ArrayList<>(1);
 
-  public LemmaFeature(PosTaggedTokenAddressFunction<T> addressFunction) {
+  public AllLemmasFeature(PosTaggedTokenAddressFunction<T> addressFunction) {
     super(addressFunction);
     this.setAddressFunction(addressFunction);
     attributes.add(LexicalAttribute.Lemma.toString());
   }
 
   @Override
-  protected FeatureResult<String> checkInternal(T context, RuntimeEnvironment env) throws TalismaneException {
-    PosTaggedTokenWrapper innerWrapper = this.getToken(context, env);
-    if (innerWrapper == null)
-      return null;
-    PosTaggedToken posTaggedToken = innerWrapper.getPosTaggedToken();
-    if (posTaggedToken == null)
-      return null;
-
-    FeatureResult<String> featureResult = null;
-    List<LexicalEntry> lexicalEntries = posTaggedToken.getLexicalEntries();
-    if (lexicalEntries.size() > 0) {
-      LexicalEntry lexicalEntry = lexicalEntries.get(0);
-      featureResult = this.generateResult(lexicalEntry.getLemma());
-    }
-    return featureResult;
+  protected List<String> getAttributes(PosTaggedTokenWrapper innerWrapper, RuntimeEnvironment env) {
+    return attributes;
   }
 
 }
